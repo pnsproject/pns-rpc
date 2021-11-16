e327c076c42e0884f36bbfcef5edc3bba29ea11c
Cupnfish <cupnfish@icloud.com>
2021-11-16 16:21:14 +0800

    Add `no_std` cfg attr

--- rpc-def/src/lib.rs
+++ rpc-def/src/lib.rs
@@ -1,5 +1,8 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
 use codec::Codec;
 use sp_runtime::traits::MaybeDisplay;
+use sp_std::vec::Vec;
 
 sp_api::decl_runtime_apis! {
     pub trait PnsRpcApi<AccountId, Node, Balance, Duration>
